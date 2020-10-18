194fc66b7e02272a4783fa0c198c6e9885635137
skfzyy <475162739@qq.com>
2020-10-18 22:43:04 +0800

    update make

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
-g++ init.cpp -o init.so -fPIC -shared -pthread -O3 -march=native
-g++ test.cpp -o test.so -fPIC -shared -pthread -O3 -march=native
+scriptPath=`dirname $0`
+g++ ${scriptPath}/init.cpp -o init.so -fPIC -shared -pthread -O3 -march=native
+g++ ${scriptPath}/test.cpp -o test.so -fPIC -shared -pthread -O3 -march=native
