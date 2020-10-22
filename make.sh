de100022f8facba994a7b0dbbd12c8d5148f835b
skfzyy <475162739@qq.com>
2020-10-23 00:44:42 +0800

    update transh

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
 scriptPath=`dirname $0`
+echo ${scriptPath}
 g++ ${scriptPath}/init.cpp -o ${scriptPath}/init.so -fPIC -shared -pthread -O3 -march=native
+echo "compile init.so success"
 g++ ${scriptPath}/test.cpp -o ${scriptPath}/test.so -fPIC -shared -pthread -O3 -march=native
+echo "compile test.so success"
